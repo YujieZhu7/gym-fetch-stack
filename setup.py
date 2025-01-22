f790f23468438c6cc18f401b3c4c773625fb3d92
YujieZhu7 <156827930+YujieZhu7@users.noreply.github.com>
2025-01-22 17:17:26 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='gym_fetch_stack',
       description='OpenAI Gym Fetch Stack, modified from the OpenAI robotics Mujoco fetch environments',
       zip_safe=False,
       install_requires=[
-          'numpy', 'gym', 'mujoco_py>=1.50' 'imageio'
+          'numpy', 'gym', 'mujoco_py>=1.50', 'imageio'
       ],
       package_data={'gym': [
         'assets/LICENSE.md',
